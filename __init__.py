b583f2eb0fe592189831753f7984e97bd3d586fd
Harold Bradley III <hbradleyiii@bradleystudio.net>
2015-12-11 12:41:39 -0500

    Updates __init__.py for module settings.

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+TODO: Write description.
+"""
+
+__title__ = ''
+__version__ = '0.0.1'
+__author__ = 'Harold Bradley III'
+__license__ = 'GPL 2.0'
+__copyright__ = 'Copyright 2015 Harold Bradley III'
+
+__all__ = ['Site', 'Site_Domain', 'Vhost', 'WP_Site']
+
+from ww import Site, Site_Domain, Vhost, WP_Site
---
+++ ww/__init__.py
@@ -0,0 +1,8 @@
+
+__version__ = '0.1'
+
+from site import Site
+from site_domain import Site_Domain
+from vhost import Vhost
+from wp_site import WP_Site
+
