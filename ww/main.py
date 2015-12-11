a23f099c3e682de0802b3d3bb1d3d821a16aaaac
Harold Bradley III <hbradleyiii@bradleystudio.net>
2015-12-11 14:28:14 -0500

    Adds main entry for program.

---
+++ ww/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#
+# name:             ww.py
+# author:           Harold Bradley III
+# email:            harold@bradleystudio.net
+# date:             12/11/2015
+#
+# description:      A program for managing websites
+#
+
+
+# from ext_pylib.prompt import prompt, prompt_str, warn_prompt
+import sys
+from ww import Site, Site_Domain, Vhost, WP_Site
+
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
