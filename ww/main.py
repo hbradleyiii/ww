4e79f11975cd9413e0480582c3415f18718dcb50
Harold Bradley III <hbradleyiii@bradleystudio.net>
2015-12-14 13:46:10 -0500

    Modifies main.py with updated corrections.

--- ww/main.py
+++ ww/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# name:             ww.py
+# name:             main.py
 # author:           Harold Bradley III
 # email:            harold@bradleystudio.net
 # date:             12/11/2015
@@ -9,14 +9,14 @@
 #
 
 
-# from ext_pylib.prompt import prompt, prompt_str, warn_prompt
+from ext_pylib.prompt import prompt, prompt_str, warn_prompt
 import sys
 from ww import Site, Site_Domain, Vhost, WP_Site
 
 
 def main():
     """Main entry point for the script."""
-    pass
+    print 'Not yet implemented.'
 
 if __name__ == '__main__':
     sys.exit(main())
