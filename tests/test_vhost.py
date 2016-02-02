08f129beab2a8c116b91ce03d4d6a4ffdbf9bc31
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-02-01 20:34:15 -0500

    Updates testing structure of vhost.

--- tests/test_vhost.py
+++ tests/test_vhost.py
@@ -13,33 +13,26 @@
 import pytest
 from ww.ww import Vhost
 
-
-def test_vhost_init():
-    """TODO: Test vhost init method."""
-    pass
-
-def test_vhost_create():
-    """TODO: Test vhost create method."""
-    pass
-
-# Monkey Patch function for read() method
-def read():
-    return """This is a sample vhost file.
+VHOST_DATA = """This is a sample vhost file.
 DocumentRoot /the/root/dir
 ErrorLog "/the/error/log"
 CustomLog "/the/access/log"
 """
 
-def test_vhost_setup_parse():
-    """Test vhost parse method."""
+def test_vhost_init():
+    """TODO: Test vhost init method."""
     vhost = Vhost('domain.com', {'path' : '/tmp/path'})
-    vhost.read = read
+    vhost.data = VHOST_DATA
     assert vhost.htdocs == '/the/root/dir'
     assert vhost.access_log == '/the/access/log'
     assert vhost.error_log == '/the/error/log'
 
-def test_vhost_get_parsed():
-    """TODO: Test vhost get_parsed method."""
+def test_vhost_create():
+    """TODO: Test vhost create method."""
+    pass
+
+def test_vhost_parse():
+    """Test vhost parse method."""
     pass
 
 def test_vhost_verify():
