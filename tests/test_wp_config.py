b9698a1a34eb2c60ac03396f57d9d1761dbb4155
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-02-01 22:14:08 -0500

    Adds tests and functionality to wp_config.

---
+++ tests/test_wp_config.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+#
+# name:             test_wp_website.py
+# author:           Harold Bradley III
+# email:            harold@bradleystudio.net
+# created on:       02/01/2016
+#
+# description:      A unit test for ww module's WPConfig class and methods.
+#
+
+import mock
+import pytest
+from ww.ww import WPConfig, WPSalt
+from ww import settings as s
+
+
+SALTS = """define('AUTH_KEY',         'm`-Di%CcLS>E(V^KUtN?HIq)+GDQ{RpbJ4N~k =Hw[%-{Zt+oHO>JdO!uW-_5,v^');
+define('SECURE_AUTH_KEY',  '5c 8cG8kfJ&0!e$x:GE ,jkB/U+tdND[Y`|MjgVA+qQI%.x/qu9)~cfQmb<>H~SU');
+define('LOGGED_IN_KEY',    '3Surb*jDad=4(cVPNPM=PJl]&2BnU AJXU4p^EVG,cci%mHxq|MF s1@![=4 ajh');
+define('NONCE_KEY',        '8&G*[f=py#+B!8!nIzpqd)B0r;s60]-tx8yWaRJ--2A-)NF4YxV_H`a[?w!iu|rT');
+define('AUTH_SALT',        '.ed^{U[~- l^D4hR5^Q/,&b(21L{G04L,*k5v&{~*BbO%#DN xfw@3lVOv]klf--');
+define('SECURE_AUTH_SALT', 'q%osy^V&j7#0tQtYbc?Ndr0PFZ1POujHfg+sgDA4Az#WZ|wFG%/2O+N)+>EC[Yd~');
+define('LOGGED_IN_SALT',   'i~O~sCX+V4;|d6gUw)M8-]Mpj!|f#ypD5ZFos4|XJbrnQddaVe+[GP83PO@jZVo#');
+define('NONCE_SALT',       '1S`>!Q@b2A+vpU-a:A=fqvKbv!*>d+AL|;&QcR!lsR+DO@L)fE;Qh>gz!~>vy5EK');
+"""
+
+def xtest_wpsalt_chars():
+    """Tests wpsalt for correct length of chars."""
+    salt = WPSalt()
+    assert len(salt.secure_auth_key) == 64
+    assert len(salt.logged_in_key) == 64
+    assert len(salt.nonce_key) == 64
+    assert len(salt.auth_salt) == 64
+    assert len(salt.secure_auth_salt) == 64
+    assert len(salt.logged_in_salt) == 64
+    assert len(salt.nonce_salt) == 64
+
+def xtest_wpsalt_assignment():
+    """Tests wpsalt for assignment using multiple salts."""
+    original_salt = WPSalt()
+    static_salt = WPSalt()
+    static_salt.data = SALTS
+    for key, value in static_salt.secrets():
+        setattr(original_salt, key, value)
+
+    assert original_salt.secure_auth_key == static_salt.secure_auth_key
+    assert original_salt.logged_in_key == static_salt.logged_in_key
+    assert original_salt.nonce_key == static_salt.nonce_key
+    assert original_salt.auth_salt == static_salt.auth_salt
+    assert original_salt.secure_auth_salt == static_salt.secure_auth_salt
+    assert original_salt.logged_in_salt == static_salt.logged_in_salt
+    assert original_salt.nonce_salt == static_salt.nonce_salt
+
+def test_wpconfig_init():
+    """Tests wpconfig initialization."""
+    config = WPConfig({ 'wp' : {
+                        'table_prefix' : 'wp_',
+                        'debug'        : 'true',
+                        'db_name'      : 'the_dbname',
+                        'db_user'      : 'the_username',
+                        'db_password'  : 'the_password',
+                        'db_host'      : 'localhost', } } )
+    assert config.table_prefix == 'wp_'
+    assert config.debug == 'true'
+    assert config.db_name == 'the_dbname'
+    assert config.db_user == 'the_username'
+    assert config.db_password == 'the_password'
+    assert config.db_host == 'localhost'
+    config.debug = 'false'
+    assert config.debug == 'false'
+    config.db_name = 'a_new_db'
+    assert config.db_name == 'a_new_db'
+
+    assert len(config.secure_auth_key) == 64
+    assert len(config.logged_in_key) == 64
+    assert len(config.nonce_key) == 64
+    assert len(config.auth_salt) == 64
+    assert len(config.secure_auth_salt) == 64
+    assert len(config.logged_in_salt) == 64
+    assert len(config.nonce_salt) == 64
+
+def test_wpconfig_parse():
+    """Test wpconfig parse()."""
+    with mock.patch('__builtin__.raw_input', return_value='n'):
+        config = WPConfig({
+            'path' : s.WP_CONFIG_TEMPLATE,
+            'wp' : {
+                'debug'        : 'true',
+                'table_prefix' : 'xyz',
+                'db_name'      : 'the_dbname',
+                'db_user'      : 'the_user',
+                'db_password'  : 'the_password',
+                'db_host'      : 'the_host',
+                'disallow_edit': 'false',
+                'fs_method'    : 'whatever', } } )
+
+    with mock.patch('__builtin__.raw_input', return_value='y'):
+        config_2 = WPConfig({'path' : s.WP_CONFIG_TEMPLATE})
+
+    result_in_memory = {
+        'debug'        : 'true',
+        'table_prefix' : 'xyz',
+        'db_name'      : 'the_dbname',
+        'db_user'      : 'the_user',
+        'db_password'  : 'the_password',
+        'db_host'      : 'the_host',
+        'disallow_edit': 'false',
+        'fs_method'    : 'whatever', }
+
+    result_on_disk = {
+        'debug'        : 'false',
+        'table_prefix' : 'wp_',
+        'db_name'      : 'db_name',
+        'db_user'      : 'db_user',
+        'db_password'  : 'db_password',
+        'db_host'      : 'localhost',
+        'disallow_edit': 'true',
+        'fs_method'    : 'direct', }
+
+    assert config.parse() == result_in_memory
+    assert config.parse(True) == result_on_disk
+    assert config_2.parse() == result_on_disk
+
+def test_wpconfig_verify():
+    """TODO:"""
+    pass
--- ww/wp_config.py
+++ ww/wp_config.py
@@ -51,8 +51,8 @@ WP_CONF_REGEXES = dict(SALT_REGEXES, **{
                       "define('DB_USER', '{}');"),
     'db_password'  : ("define\('DB_PASSWORD', '(.*)'\);",
                       "define('DB_PASSWORD', '{}');"),
-    'db_hostname'  : ("define\('DB_HOSTNAME', '(.*)'\);",
-                      "define('DB_HOSTNAME', '{}');"),
+    'db_host'      : ("define\('DB_HOST', '(.*)'\);",
+                      "define('DB_HOST', '{}');"),
     'table_prefix' : ("\$table_prefix[ ]*=[ ]*['\"](.*)['\"];",
                       "$table_prefix = '{}';"),
     'disallow_edit': ("define\('DISALLOW_FILE_EDIT', (.*)\);",
@@ -92,10 +92,11 @@ class WPConfig(Parsable, WWFile):
     regexes = WP_CONF_REGEXES
 
     def __init__(self, atts):
-        self.setup_parsing()  # Do this FIRST, before initializing attributes
+        self.setup_parsing()  # Do this first, before initializing attributes
+
         super(WPConfig, self).__init__(atts)
         if self.exists() and prompt('Parse existing wp_config.php?'):
-            atts = self.parse()
+            atts['wp'] = self.parse(True)
         else:  # Otherwise, apply the template
             self.template = WPConfigTemplate({'path' : s.WP_CONFIG_TEMPLATE}).read()
             self.data = self.template
@@ -104,64 +105,150 @@ class WPConfig(Parsable, WWFile):
         for key, value in self.salt.secrets():
             setattr(self, key, value)
 
-        self.table_prefix = atts['wp']['table_prefix']
-        self.debug = atts['wp']['debug']
-        self.db_name = atts['wp']['db_name']
-        self.db_user = atts['wp']['db_user']
-        self.db_password = atts['wp']['db_password']
-        self.db_hostname = atts['wp']['db_hostname']
+        if 'wp' in atts:
+            for att, value in atts['wp'].iteritems():
+                setattr(self, att, value)
 
-        self.disallow_edit = 'true'
-        self.fs_method = 'direct'
 
-    def parse(self):
-        """This does not return the salts. They will be changed every time."""
-        if not self.debug:
-            print 'Could not parse debug mode.'
-            if prompt('Turn debug mode on?'):
-                self.debug = 'true'
-            else:
-                self.debug = 'false'
+    def parse(self, parse_from_disk=False):
+        """Returns a dict of attributes.  This does not return the salts. They
+           will be changed every time.  Passing in True as an arguement will
+           force reading from disk."""
+
+        if parse_from_disk:
+            """Prompting is necessary (but hopefully unusual) if file on disk
+               cannot be parsed."""
+
+            self.read(True)  # Force read() clearing memory
+
+            if not self.debug:
+                print 'Could not parse debug mode.'
+                if prompt('Turn debug mode on?'):
+                    self.debug = 'true'
+                else:
+                    self.debug = 'false'
+
+            if not self.table_prefix:
+                print 'Could not parse WordPress database table_prefix.'
+                self.table_prefix = prompt_str('What is the WordPress database table_prefix?')
+
+            if not self.db_name:
+                print 'Could not parse WordPress database name.'
+                self.db_name = prompt_str('What is the WordPress database name?')
+
+            if not self.db_user:
+                print 'Could not parse WordPress database user.'
+                self.db_user = prompt_str('What is the WordPress database user?')
+
+            if not self.db_password:
+                print 'Could not parse WordPress database password.'
+                self.db_password = prompt_str('What is the WordPress database password?')
+
+            if not self.db_host:
+                print 'Could not parse WordPress database hostname.'
+                self.db_host = prompt_str('What is the WordPress database hostname?')
+
+            if not self.disallow_edit:
+                print 'Could not parse DISALLOW_EDIT.'
+                if prompt('Set DISALLOW_EDIT to "true"?'):
+                    self.disallow_edit = 'true'
+                else:
+                    self.disallow_edit = 'false'
+
+            if not self.fs_method:
+                print 'Could not parse fs_method.'
+                self.fs_method = prompt_str('What should we set fs_method?', 'direct')
+
         else:
-            self.debug = self.debug[0]
+            self.read()  # At least make sure contents are already read into
+                         # memory.
 
-        if not self.table_prefix:
-            print 'Could not parse WordPress database table_prefix.'
-            self.table_prefix = prompt_str('What is the WordPress database table_prefix?')
-        else:
-            self.table_prefix = self.table_prefix[0]
-
-        if not self.db_name:
-            print 'Could not parse WordPress database name.'
-            self.db_name = prompt_str('What is the WordPress database name?')
-        else:
-            self.db_name = self.db_name[0]
-
-        if not self.db_user:
-            print 'Could not parse WordPress database user.'
-            self.db_user = prompt_str('What is the WordPress database user?')
-        else:
-            self.db_user = self.db_user[0]
-
-        if not self.db_password:
-            print 'Could not parse WordPress database password.'
-            self.db_password = prompt_str('What is the WordPress database password?')
-        else:
-            self.db_password = self.db_password[0]
-
-        if not self.db_hostname:
-            print 'Could not parse WordPress database hostname.'
-            self.db_hostname = prompt_str('What is the WordPress database hostname?')
-        else:
-            self.db_hostname = self.db_hostname[0]
-
-        return { 'debug'        : self.htdocs,
-                 'table_prefix' : self.db_name,
+        return { 'debug'        : self.debug,
+                 'table_prefix' : self.table_prefix,
                  'db_name'      : self.db_name,
                  'db_user'      : self.db_user,
                  'db_password'  : self.db_password,
-                 'db_hostname'  : self.db_hostname, }
+                 'db_host'      : self.db_host,
+                 'disallow_edit': self.disallow_edit,
+                 'fs_method'    : self.fs_method, }
 
-    def verify(self):
-        # [WARN] debug is set True ... etc.
-        pass
+    def verify(self, repair=False):
+        """This assumes the in-memory values are the correct values."""
+        result = True  # Assume the best :)
+
+        correct_values = self.parse()  # Retrieve values in memory (and hold
+                                       # for comparison and/or repair)
+        self.read(True)  # Read values in from disk
+
+        if not self.debug == correct_values['debug']:
+            print '[!] Debug is incorrectly set to: ' + self.debug
+            if repair:
+                print 'Setting debug to: ' + correct_values['debug']
+                self.debug = correct_values['debug']
+            else:
+                result = False
+
+        if not self.table_prefix == correct_values['table_prefix']:
+            print '[!] WordPress table prefix is incorrectly set to: ' + self.table_prefix
+            if repair:
+                print 'Setting table prefix to: ' + correct_values['table_prefix']
+                self.table_prefix = correct_values['table_prefix']
+            else:
+                result = False
+
+        if not self.db_name == correct_values['db_name']:
+            print '[!] WordPress database is incorrectly set to: ' + self.db_name
+            if repair:
+                print 'Setting database name to: ' + correct_values['db_name']
+                self.db_name = correct_values['db_name']
+            else:
+                result = False
+
+        if not self.db_user == correct_values['db_user']:
+            print '[!] WordPress database username is incorrectly set to: ' + self.db_user
+            if repair:
+                print 'Setting database user to: ' + correct_values['db_user']
+                self.db_user = correct_values['db_user']
+            else:
+                result = False
+
+        if not self.db_password == correct_values['db_password']:
+            print '[!] WordPress database password is incorrectly set to: ' + self.db_password
+            if repair:
+                print 'Setting database password to: ' + correct_values['db_password']
+                self.db_password = correct_values['db_password']
+            else:
+                result = False
+
+        if not self.db_host == correct_values['db_host']:
+            print '[!] Databse hostname is incorrectly set to: ' + self.db_host
+            if repair:
+                print 'Setting database host to: ' + correct_values['db_host']
+                self.db_host = correct_values['db_host']
+            else:
+                result = False
+
+        if not self.disallow_edit == correct_values['disallow_edit']:
+            print '[!] DISALLOW_EDIT is incorrectly set to: ' + self.disallow_edit
+            if repair:
+                print 'Setting disallow_edit to: ' + correct_values['disallow_edit']
+                self.disallow_edit = correct_values['disallow_edit']
+            else:
+                result = False
+
+        if not self.fs_method == correct_values['fs_method']:
+            print '[!] FS_METHOD is incorrectly set to: ' + self.fs_method
+            if repair:
+                print 'Setting fs_method to: ' + correct_values['fs_method']
+                self.fs_method = correct_values['fs_method']
+            else:
+                result = False
+
+        if self.debug == 'true':
+            print '-------------------------------------'
+            print '[WARN] WordPress Debug mode is on.'
+            print '       Be sure to turn this off in'
+            print '       a production environement!'
+            print '-------------------------------------'
+
+        return result
