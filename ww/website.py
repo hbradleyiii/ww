318033e17963073586f38d861a60ed86786e553d
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-02-24 13:18:00 -0500

    Corrects some inconsistencies and clarifies the purpose of init.

--- ww/website.py
+++ ww/website.py
@@ -6,6 +6,7 @@
 # email:            harold@bradleystudio.net
 # date:             11/11/2015
 #
+# pylint:           disable=line-too-long
 
 """
 ww.website
@@ -69,12 +70,12 @@ def merge_atts(atts, new_atts):
     return atts
 
 
-class Website(object):
+class Website(object):  # pylint: disable=too-many-instance-attributes
     """A class that describes a generic website with the following properties:
         domain      Website domain
         htdocs      Website root directory
         assets      Website assets directory
-        logs        Website log directory
+        log         Website log directory
         access_log  Website access log file
         error_log   Website error log file
         vhost       Apache vhost config file
@@ -110,20 +111,20 @@ class Website(object):
                 'owner' : 'root',
                 'group' : s.WWW_ADMIN,
             },
-            'logs' : {
+            'log' : {
                 'path'  : s.WWW_DIR + self.domain + '/log/',
                 'perms' : 0770,
                 'owner' : 'root',
                 'group' : s.WWW_ADMIN,
             },
             'access_log' : {
-                'path'  : None,
+                'path'  : s.WWW_DIR + self.domain + '/log/access_log',
                 'perms' : 0750,
                 'owner' : 'root',
                 'group' : s.WWW_ADMIN,
             },
             'error_log' : {
-                'path'  : None,
+                'path'  : s.WWW_DIR + self.domain + '/log/error_log',
                 'perms' : 0750,
                 'owner' : 'root',
                 'group' : s.WWW_ADMIN,
@@ -140,7 +141,7 @@ class Website(object):
                 'email'      : s.SITE_ADMIN_EMAIL,
             },
             'htaccess' : {
-                'path'  : None,
+                'path'  : s.WWW_DIR + self.domain + '/htdocs/.htaccess',
                 'perms' : 0664,
                 'owner' : s.WWW_USR,
                 'group' : s.WWW_USR,
@@ -152,28 +153,13 @@ class Website(object):
 
         atts = merge_atts(default_atts, atts)
 
-        self.vhost = Vhost(self.domain.name, atts['vhost'])
-
-        # If an apache vhost config already exists, try to parse it
-        if self.vhost.exists():
-            print(str(self.vhost) + ' already exists.')
-            if prompt('Parse existing vhost configuration?'):
-                atts = merge_atts(atts, self.vhost.parse())
-        # TODO: Convert...or migrate?
-
+        # Initialize Directories
         self.root = Dir(atts['root'])
         self.htdocs = Dir(atts['htdocs'])
         self.assets = Dir(atts['assets'])
-        self.logs = Dir(atts['logs'])
+        self.log = Dir(atts['log'])
 
-        # Set path of htaccess, access_log, and error_log after htdocs is set.
-        if not atts['htaccess']['path']:
-            atts['htaccess']['path'] = self.htdocs.path + '.htaccess'
-        if not atts['access_log']['path']:
-            atts['access_log']['path'] = self.logs.path + s.SITE_ACCESS_LOG
-        if not atts['error_log']['path']:
-            atts['error_log']['path'] = self.logs.path + s.SITE_ERROR_LOG
-
+        # Initialize Files
         self.htaccess = Htaccess(atts['htaccess'])
         self.access_log = File(atts['access_log'])
         self.error_log = File(atts['error_log'])
@@ -188,20 +174,20 @@ class Website(object):
         string += '\n  vhost config:     ' + str(self.vhost)
         string += '\n  htdocs directory: ' + str(self.htdocs)
         string += '\n  assets:           ' + str(self.assets)
-        string += '\n  logs:             ' + str(self.logs)
+        string += '\n  log:              ' + str(self.log)
         string += '\n----------------------------------------------------------\n'
         return string
 
     def __repr__(self):
         """Returns a python string that evaluates to the object instance."""
-        return "{0}('{1}', {'htdocs' : {2}, 'assets' : {3}, 'logs' : {4}, " + \
+        return "{0}('{1}', {'htdocs' : {2}, 'assets' : {3}, 'log' : {4}, " + \
                "'access_log' : {5}, 'error_log' : {6}, 'htaccess' : {7}, " + \
                "'vhost' : {8}})".format(
                    self.__class__.__name__,
                    self.domain,
                    self.htdocs.get_atts(),
                    self.assets.get_atts(),
-                   self.logs.get_atts(),
+                   self.log.get_atts(),
                    self.access_log.get_atts(),
                    self.error_log.get_atts(),
                    self.htaccess.get_atts(),
@@ -217,7 +203,7 @@ class Website(object):
         self.root.create()
         self.htdocs.create()
         self.assets.create()
-        self.logs.create()
+        self.log.create()
         self.access_log.create()
         self.error_log.create()
         self.htaccess.create()
@@ -237,7 +223,7 @@ class Website(object):
         self.access_log.remove(ask)
         self.error_log.remove(ask)
         self.htaccess.remove(ask)
-        self.logs.remove(ask)
+        self.log.remove(ask)
         self.htdocs.remove(ask)
         self.assets.remove(ask)
         self.root.remove(ask)
@@ -268,7 +254,7 @@ class Website(object):
                       self.access_log.verify(repair),
                       self.error_log.verify(repair),
                       self.htaccess.verify(repair),
-                      self.logs.verify(repair),
+                      self.log.verify(repair),
                       self.htdocs.verify(repair),
                       self.assets.verify(repair),
                       self.root.verify(repair),
