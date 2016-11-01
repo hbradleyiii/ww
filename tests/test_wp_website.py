0bb46c795fcdf0be873af9c9f3358d1b0f231b29
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-11-01 14:18:35 -0400

    Adds print function from future.

--- tests/test_wp_website.py
+++ tests/test_wp_website.py
@@ -12,7 +12,7 @@
 A unit test for ww module's WPWebsite class and methods.
 """
 
-from __future__ import absolute_import
+from __future__ import absolute_import, print_function
 
 from mock import patch
 
